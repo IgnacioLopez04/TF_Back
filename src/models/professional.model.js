e55153d142939f81f732e32477195b9e23ca9272
Ignacio <ignacioelopez04@gmail.com>
2025-01-23 16:36:54 -0300

    ft: ABM professional and speciality

--- src/models/professional.model.js
+++ src/models/professional.model.js
@@ -25,8 +25,57 @@ export class professionalModel {
          );
       }
    }
-   static async getProfessional() {
+   //! Ver si hace falta refactorizar
+   static async getProfessional(id_profesional) {
       try {
+         const professional = await pool.query(
+            `
+               SELECT * 
+               FROM profesional
+               WHERE id_profesional = $1
+            `,
+            [id_profesional],
+         );
+
+         const specialities = await pool.query(
+            `
+               SELECT * 
+               FROM especialidad_profesional 
+               WHERE id_profesional = $1
+            `,
+            [id_profesional],
+         );
+
+         const specialitiesPromise = specialities.rows.map(
+            async (speciality) => {
+               const response = await pool.query(
+                  `
+                  SELECT *
+                  FROM especialidad
+                  WHERE id_especialidad = $1
+               `,
+                  [speciality.id_especialidad],
+               );
+
+               return response.rows[0];
+            },
+         );
+         const specialityComplete = Promise.all(specialitiesPromise);
+
+         const user = await pool.query(
+            `
+               SELECT * 
+               FROM usuario
+               WHERE id_usuario = $1
+            `,
+            [professional.rows[0].id_usuario],
+         );
+
+         return {
+            user: user.rows[0],
+            id_profesional,
+            specialities: specialityComplete,
+         };
       } catch (err) {
          throw new DefaultError(
             'DatabaseError',
@@ -35,8 +84,58 @@ export class professionalModel {
          );
       }
    }
+   //! Ver si hace falta refactorizar
    static async getAllProfessionals() {
       try {
+         const professionals = await pool.query(`
+               SELECT * 
+               FROM profesional
+            `);
+
+         const professionalsPromise = professionals.rows.map(
+            async (professional) => {
+               const user = await pool.query(
+                  `
+                     SELECT * 
+                     FROM usuario
+                     WHERE id_profesional = $1
+                  `,
+                  [professional.id_usuario],
+               );
+               const specialities = await pool.query(
+                  `
+                     SELECT * 
+                     FROM profesional_especialidad 
+                     WHERE id_profesional = $1
+                  `,
+                  [professional.id_profesional],
+               );
+               const specialitiesPromise = specialities.rows.map(
+                  async (speciality) => {
+                     const response = await pool.query(
+                        `
+                        SELECT * 
+                        FROM especialidad
+                        WHERE id_especialidad = $1
+                     `,
+                        [speciality.id_especialidad],
+                     );
+                     return response.rows[0];
+                  },
+               );
+               const specialitiesComplete = Promise.all(specialitiesPromise);
+
+               return {
+                  user: user.rows[0],
+                  specialities: specialitiesComplete,
+                  id_profesional: professional.id_profesional,
+               };
+            },
+         );
+
+         const professionalsComplete = Promise.all(professionalsPromise);
+
+         return professionalsComplete;
       } catch (err) {
          throw new DefaultError(
             'DatabaseError',
@@ -45,8 +144,15 @@ export class professionalModel {
          );
       }
    }
-   static async insertSpeciality() {
+   static async insertSpeciality(description) {
       try {
+         const response = await pool.query(
+            `
+               INSERT INTO especialidad(descripcion)
+               VALUES($1)
+            `,
+            [description],
+         );
       } catch (err) {
          throw new DefaultError(
             'DatabaseError',
@@ -55,8 +161,17 @@ export class professionalModel {
          );
       }
    }
-   static async getSpeciality() {
+   static async getSpeciality(id_especialidad) {
       try {
+         const speciality = await pool.query(
+            `
+               SELECT * 
+               FROM especialidad 
+               WHERE id_especialidad = $1
+            `,
+            [id_especialidad],
+         );
+         return speciality.rows[0];
       } catch (err) {
          throw new DefaultError(
             'DatabaseError',
@@ -67,6 +182,11 @@ export class professionalModel {
    }
    static async getAllSpecialities() {
       try {
+         const speciality = await pool.query(`
+               SELECT * 
+               FROM especialidad
+            `);
+         return speciality.rows;
       } catch (err) {
          throw new DefaultError(
             'DatabaseError',
