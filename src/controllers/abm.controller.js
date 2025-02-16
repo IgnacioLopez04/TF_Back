baa06f9d3b8a1e9b3507d453930b2e60463e94ff
Ignacio <ignacioelopez04@gmail.com>
2025-02-16 19:27:21 -0300

    add: ABM

---
+++ src/controllers/abm.controller.js
@@ -0,0 +1,2 @@
+// TODO - Aca voy a colocar todos los ABM de aquellas tablas que sea secundarias y pero necesarias para el sistema
+// Mutual, Datos_mutual, Provincia, Ciudad, Barrio, codigo_postal, calle, prestacion, modulo, etc.
