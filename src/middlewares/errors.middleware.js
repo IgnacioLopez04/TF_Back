7c714f157857a87f9cf74c1a552a43c842101892
Ignacio <ignacioelopez04@gmail.com>
2025-11-19 16:50:18 -0300

    chore: cambios menores

--- src/middlewares/errors.middleware.js
+++ src/middlewares/errors.middleware.js
@@ -1,7 +1,8 @@
 export const errorHandler = (error, req, res, next) => {
-   res.status(error.statusCode).json({
-      status: error.statusCode,
-      message: error.message,
-      stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
+   const statusCode = error.statusCode || 500;
+   res.status(statusCode).json({
+     status: statusCode,
+     message: error.message || 'Internal Server Error',
+     stack: process.env.NODE_ENV === 'production' ? '' : error.stack,
    });
 };
